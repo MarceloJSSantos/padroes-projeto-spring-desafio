b3162daf35c969fb9a1e4e5e3dc0631716892003
MarceloJSSantos <marcelojssantos2012@gmail.com>
2022-10-26 15:10:49 -0300

    Padrões de Projeto com Spring (desafio) - implementação dos métodos da ClienteServiceImpl

--- src/main/java/br/marcelojssantos/padroesprojetospring/service/ClienteServiceImpl.java
+++ src/main/java/br/marcelojssantos/padroesprojetospring/service/ClienteServiceImpl.java
@@ -1,32 +1,61 @@
 package br.marcelojssantos.padroesprojetospring.service;
 
 import br.marcelojssantos.padroesprojetospring.model.Cliente;
+import br.marcelojssantos.padroesprojetospring.model.ClienteRepository;
+import br.marcelojssantos.padroesprojetospring.model.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService{
+
+    @Autowired
+    ClienteRepository clienteRepository;
+    @Autowired
+    EnderecoRepository enderecoRepository;
+    @Autowired
+    ViaCepService viaCepService;
+
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        var cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-
+        salvarClienteComCep(cliente);
     }
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
-
+        var clienteBD = clienteRepository.findById(id);
+        if(clienteBD.isPresent()){
+            cliente.setId(id);
+            salvarClienteComCep(cliente);
+        }
     }
 
     @Override
     public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
 
+    private void salvarClienteComCep(Cliente cliente) {
+        var cepCliente = cliente.getEndereco().getCep();
+        var endereco = enderecoRepository.findById(cepCliente).orElseGet(() ->{
+            var novoEndereco = viaCepService.consultarCep(cepCliente);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 }
